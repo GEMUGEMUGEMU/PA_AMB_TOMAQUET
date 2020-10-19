6813f86c3b376932266d2f7aabbf7f6488a94765
Andrea Andreu Salvagnin <andrea.salvagnin@protonmail.com>
2020-10-19 15:58:34 +0200

    kinetik object added

---
+++ GameObjects/AnimatedKineticObject.cpp
@@ -0,0 +1,13 @@
+/*
+*        ~_
+*     ~_ )_)~_
+*     )_))_))_)
+*     _!__!__!_
+*   ~~\t  Gemu/~~
+*
+*  File Name: AnimatedKineticObject.cpp
+*  Purpose: 
+*  Creation Date: 15-10-20
+*  Created By: Andrea Andreu Salvagnin
+*/
+
---
+++ GameObjects/AnimatedKineticObject.h
@@ -0,0 +1,19 @@
+/*
+*        ~_
+*     ~_ )_)~_
+*     )_))_))_)
+*     _!__!__!_
+*   ~~\t  Gemu/~~
+*
+*  File Name: AnimatedKineticObject.h
+*  Purpose: 
+*  Creation Date: 15-10-20
+*  Created By: Andrea Andreu Salvagnin
+*/
+
+#ifndef AnimatedKineticObject_h
+#define AnimatedKineticObject_h
+
+class AnimatedKineticObject : public Game
+
+#endif /* AnimatedKineticObject_h */
